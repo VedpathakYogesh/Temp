fb2bfdb3330ddba443b36535f3ccae049bb4ee96
YOGESHVEDPATHAK333 <yogeshvedpathak333@gmail.com>
2017-01-31 18:00:20 +0530

    "LogExceptionUsingDatabase"

---
+++ Log4netToDb/Log4netToDb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Log4netToDb.Startup))]
+namespace Log4netToDb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
