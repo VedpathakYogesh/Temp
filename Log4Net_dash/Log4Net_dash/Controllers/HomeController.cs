0f98a290dc034d63e3f6e861d2f8ee969379ee0b
YOGESHVEDPATHAK333 <yogeshvedpathak333@gmail.com>
2017-02-01 13:07:36 +0530

    "LogExceptioDb"

---
+++ Log4Net_dash/Log4Net_dash/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using log4net;
+using log4net.Config;
+
+namespace Log4Net_dash.Controllers
+{
+    public class HomeController : Controller
+
+    {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static HomeController()
+        {
+            XmlConfigurator.Configure();
+        }
+
+        public ActionResult Index()
+        {
+            log.DebugFormat("log item [{0}]", 1);
+            log.DebugFormat("log item [{0}]", 2);
+            log.DebugFormat("log item [{0}]", 3);
+            log.DebugFormat("log item [{0}]", 4);
+            log.DebugFormat("log item [{0}]", 5);
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Log4Net_dash/Log4Net_dash/Models/Log.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Log4Net_dash.Models
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Level { get; set; }
+        public string Logger { get; set; }
+        public string Message { get; set;}
+        public string Exception { get; set;}
+
+    }
+}
